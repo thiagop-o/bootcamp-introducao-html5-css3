df0943536c903be0171c950a91ce4d34c046a7c0
Thiago Oliveira <thiagop_o@hotmail.com>
2021-06-09 22:14:33 -0300

    Acidionado a Aula de Introdução ao JavaScript

---
+++ programacao_javascript/js/main.js
@@ -0,0 +1,90 @@
+function botao(){
+    
+    document.getElementById("agradecimento").innerHTML = "Obrigado por clicar";
+    //window.open("https://google.com.br");
+}
+
+
+function trocar(){
+    document.getElementById("agradecimento").innerHTML = "Obrigado por passar o mouse";
+}
+
+/*
+function soma(n1,n2){
+    return n1+n2;
+}
+
+
+function validaIdade(idade){
+    let validar;
+    
+    if (idade >= 18){
+        validar = true;
+    }else{
+        validar = false;
+    }
+    
+    return validar;
+}
+
+
+
+console.log(soma(5,10));
+console.log(validaIdade(18));
+*/
+/*var d = new Date();
+console.log(d);
+console.log(d.getFullYear());
+
+var var1 = "arpzz";
+var var2 = "bfefas";
+console.log(var1 + var2  + 1425 + "-" + 1519);*/
+
+/*var count = 0;
+while (count < 5) {
+    count++;
+    console.log(count);
+}*/
+
+/*var count;
+for(count = 0; count <=5; count++){
+    console.log(count);
+}
+
+
+/*var idade = prompt("Qual a sua idade?");
+if(idade > 18){
+    alert("Maior de idade");
+}else{
+    alert("Menor de idade");
+}*/
+
+
+/*var fruta = {
+    nome: "maça",
+    cor: "vermelha"
+
+};
+
+console.log(fruta.cor);*/
+
+
+
+/*var lista = ["maça", "pera", "laranja"];
+lista.push("uva");
+//lista.pop();
+console.log(lista);
+console.log(lista.toString());*/
+
+
+
+
+/*var nome = "Thiago Oliveira";
+var idade1 = 34;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo";
+
+//alert("Nome " + nome + " tem " + idade);
+//alert(idade1 + idade2);
+console.log(idade1 + idade2);
+console.log(frase.replace("Japão","Alemanha").toLowerCase());*/
